84cd92f38b191ab7303190438fe5f6ea9beddc06
Jeancarlos  Carvajal <jeancarloscarvajalavila@gmail.com>
2022-06-23 15:27:49 -0400

    Respuesta de la Api formateada usando la erramienta de https://app.quicktype.io/ que crea una clase para dar formato a una respuesta JSON de gran magnitud

--- lib/providers/movies_provider.dart
+++ lib/providers/movies_provider.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http; 
+import 'package:http/http.dart' as http;
+import 'package:peliculas/models/now_playing_response.dart'; 
 import '/apis/api_keys.dart';
-
+import 'dart:convert'; // se usa para manejar los json
 class MoviesProvider extends ChangeNotifier { // agrege  extends ChangeNotifier  para que pueda funcionar
 
-  final String _apiKey =  ApiConfig.apiKey; // Api key desde la clase de git ignore
+  final String _apiKey =  ApiConfig.apiKey; // Api key desde la clase de git ignore en archivo /apis/api_keys.dart
   final String _baseUrl = 'api.themoviedb.org';
   final String _language = 'es-ES';
 
@@ -27,6 +28,13 @@ class MoviesProvider extends ChangeNotifier { // agrege  extends ChangeNotifier 
 
     // Await the http get response, then decode the json-formatted response.
     final response = await http.get(url);
+    // if(response.statusCode != 200) return print('error');
+    // Map<String, dynamic> especifico el tipo de datos que va recibir
+    // final Map<String, dynamic> decodeData = json.decode(response.body); // json.decode proviene de paquete import dart:convert
+    final nowPlayingResponse = NowPlayingResponse.fromJson(response.body);
+
+    // print(decodeData);
+    print(nowPlayingResponse.results[1].title);
 
   }
 }
